e12842dbc23532974eb0caef696425cce8821556
shige0309 <qqa27yed@chic.ocn.ne.jp>
2023-09-26 12:00:30 +0900

    🐛 MV修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -132,7 +132,7 @@ export default function Home() {
         color="white"
         setRef={elementsObject.last?.ref}
       >
-        <div className="height100 brownBgColor flex justify-center items-center w-full z-0 top-0 left-0 fixed">
+        <div className="h-[90dvh] sm:h-[100vh] brownBgColor flex justify-center items-center w-full z-0 top-0 left-0 fixed">
           <div>
             <div
               className={`${
@@ -213,7 +213,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="height90"></div>
+        <div className="h-[80dvh] sm:h-[90vh]"></div>
         <Parallax
           bgImage={"/img/top/attention-img1.jpg"}
           strength={200}
